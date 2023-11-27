79c3d8040ca7d99ce8021fca0e5fd6b101b98d28
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-11-27 09:51:27 -0800

    .

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello world!");
-weatherCard = document.querySelector("weather-card");
-
-weatherCard.innerHtml = "HI THERE";
-weatherCard.innerHtml = "HI THERE";
-weatherCard.innerHtml = "HI THERE";
-weatherCard.innerHtml = "HI THERE";
